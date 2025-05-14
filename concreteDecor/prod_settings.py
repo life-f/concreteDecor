ce7f7b718332d17007d1ca03919e05c1e5ae80ef
life-f <58433392+life-f@users.noreply.github.com>
2025-05-14 14:25:14 +0300

    FIX for deploy

--- concreteDecor/prod_settings.py
+++ concreteDecor/prod_settings.py
@@ -12,7 +12,7 @@ DATABASES = {
         "USER": "concrete_user",
         "PASSWORD": "concrete_decor",
         "HOST": "127.0.0.1",
-        "PORT": "5433",
+        "PORT": "5432",
     }
 }
 
--- concreteDecor/urls.py
+++ concreteDecor/urls.py
@@ -16,13 +16,10 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.template.defaulttags import url
 from django.urls import path, include
-from django.views.static import serve
 
-from api.views import ActivationView
 from api.admin import admin_site
+from api.views import ActivationView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
@@ -31,9 +28,8 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('api/', include('api.urls')),
-    # url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    # url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
